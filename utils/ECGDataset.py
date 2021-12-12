761deda6622cb3aafe9a42cc1cf213072b6e17b0
hei-jung <heijung.jang@hotmail.com>
2021-12-12 22:09:24 +0900

    update ECGDataset

--- utils/ECGDataset.py
+++ utils/ECGDataset.py
@@ -3,8 +3,8 @@ import torch
 from torch.utils.data import Dataset
 
 
-class ECGDataset(Dataset):
-    def __init__(self, data_path, label_path, test=True):
+class ECGdataset(Dataset):
+    def __init__(self, data_path, label_path):
         self.X = np.load(data_path)
         self.y = np.load(label_path)
 
@@ -13,14 +13,11 @@ class ECGDataset(Dataset):
         self.X = torch.FloatTensor(self.X)  # cpu tensor
         self.y = torch.FloatTensor(self.y)  # cpu tensor
 
-        self.test = test
-
     def __len__(self):
         return len(self.y)
 
     def __getitem__(self, index):
         X = self.X[index]
-        if self.test: X = X.reshape(1, 12, 4096)
         y = self.y[index]
         return {
             'X': X,
